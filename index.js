b326bd8c8446a39e632eda515e782aa3ba0d2f00
Kabilov Daler <kabilovd7@gmail.com>
2023-05-26 17:12:12 +0300

    origin

--- index.js
+++ index.js
@@ -34,16 +34,12 @@ app.use(express.json());
   })
 ); */
 app.use((req, res, next) => {
-  //allow access to current url. work for https as well
-  cors({
-    credentials: true,
-    origin: "https://blog-front-mostahfwv-1doler.vercel.app/",
-  });
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.removeHeader("x-powered-by");
-  //allow access to current method
   res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+
   next();
 });
 
