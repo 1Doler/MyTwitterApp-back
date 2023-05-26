43e660e19a5c2cdb860667e52196c4cbbaae6fbd
Kabilov Daler <kabilovd7@gmail.com>
2023-05-26 16:17:11 +0300

    Initial commit

--- index.js
+++ index.js
@@ -35,11 +35,11 @@ app.use(express.json());
 ); */
 app.use((req, res, next) => {
   //allow access to current url. work for https as well
-  res.setHeader("Access-Control-Allow-Origin", req.header("Origin"));
+  res.setHeader("Access-Control-Allow-Origin", "*");
   res.removeHeader("x-powered-by");
   //allow access to current method
-  res.setHeader("Access-Control-Allow-Methods", req.method);
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   next();
 });
 
