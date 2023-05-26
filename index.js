c8da59138ccdc2b5a623a4f1b16f71aabbba2771
Kabilov Daler <kabilovd7@gmail.com>
2023-05-26 17:18:12 +0300

    add metod

--- index.js
+++ index.js
@@ -36,7 +36,10 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.removeHeader("x-powered-by");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH"
+  );
   res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
   res.setHeader("Access-Control-Allow-Credentials", "true");
 
