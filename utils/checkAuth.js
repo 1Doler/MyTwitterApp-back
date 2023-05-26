255dcfe1c9c38c146fe7215443ce82a2cb46edfa
Kabilov Daler <kabilovd7@gmail.com>
2023-05-26 17:59:05 +0300

    t

--- utils/checkAuth.js
+++ utils/checkAuth.js
@@ -1,6 +1,8 @@
 import jwt from "jsonwebtoken";
 
 export default (req, res, next) => {
+  console.log("res", res);
+  console.log("req", req);
   const token = (req.headers.cookie || "").replace(/token=\s?/, "");
   if (!token) {
     return res.status(403).json({
@@ -10,6 +12,8 @@ export default (req, res, next) => {
 
   try {
     const decoded = jwt.verify(token, "status");
+    console.log("req.userId", req.userId);
+    console.log("decoded._id", decoded._id);
     req.userId = decoded._id;
     next();
   } catch (err) {
